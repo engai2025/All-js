93b58cec13610a81e6a4bce60ca595d9e167b762
engai2025 <engai2025@gmail.com>
2025-06-15 07:38:51 +0100

    Restractuired folders

--- 01-Exercise/Assets/main.js
+++
@@ -1,8 +0,0 @@
-let x = 7;
-let y = 3;
-
- console.log(x+y); // 10
- console.log(x-y);
- console.log(x*y);
- console.log(x/y);
- console.log(x%y);
---
+++ 01-Exercise/main.js
@@ -0,0 +1,9 @@
+let x = 7;
+let y = 3;
+
+console.log("x + y =", x + y);     // 10 (Addition)
+console.log("x - y =", x - y);     // 4  (Subtraction)
+console.log("x * y =", x * y);     // 21 (Multiplication)
+console.log("x / y =", x / y);     // 2.333... (Division)
+console.log("x % y =", x % y);     // 1  (Modulus - remainder)
+console.log("x ** y =", x ** y);   // 343 (Exponentiation: 7^3)
